37b94041b522e6729143d9f07b1fb4870cfdb41f
dav-u <24206701+dav-u@users.noreply.github.com>
2024-06-24 10:51:05 +0200

    add world simulation

--- kalman_filter/main.py
+++ kalman_filter/main.py
@@ -9,5 +9,42 @@ x_ground_truth = np.array([
   params.START_SPEED
 ])
 
+x_history = []
+z_history = []
+
 while time < params.MAX_SIM_TIME:
-  pass
+  w = np.random.multivariate_normal(np.zeros(2), params.Q_ground_truth)
+
+  x_ground_truth = params.F_ground_truth @ x_ground_truth + \
+                   params.B_ground_truth @ params.u + w
+  x_history.append(x_ground_truth)
+
+  # simulate noisy measurement
+  v = np.random.multivariate_normal(np.zeros(2), params.R_ground_truth)
+  z = params.H_ground_truth @ x_ground_truth + v
+  z_history.append(z)
+
+  time += params.DT
+
+x_history = np.array(x_history)
+z_history = np.array(z_history)
+
+plt.subplot(4, 1, 1)
+plt.title("Position")
+plt.ylabel("time [s]")
+plt.ylabel("position [m]")
+plt.plot(x_history[:, 0], label="True position")
+plt.plot(z_history[:, 0], label="Measured position")
+plt.legend()
+
+plt.subplot(4, 1, 2)
+plt.title("Speed")
+plt.ylabel("time [s]")
+plt.ylabel("speed [m/s]")
+plt.plot(x_history[:, 1], label="True speed")
+plt.plot(z_history[:, 1], label="Measured speed")
+plt.legend()
+
+plt.subplots_adjust(hspace=1)
+
+plt.show()
--- kalman_filter/params.py
+++ kalman_filter/params.py
@@ -8,6 +8,9 @@ START_SPEED = 13.8 # meter per second
 DT = 1.0
 
 
+# Process parameters (simulating the real world)
+#____________________________________________________________________________
+
 # F_ground_truth is something that is true for the world.
 # When implementing a Kalman Filter we are not really able to change this.
 
@@ -19,3 +22,26 @@ DT = 1.0
 #                      |0.0 1.0| |speed|   |    speed     |
 F_ground_truth = np.array([[1.0, DT],
                            [0.0, 1.0]])
+
+
+# e.g. a car accelerates from 100km/h to 150km/h in one hour
+# a [m/s^2] = delta_speed / delta_time = 50,000m / (3600^2)s^2 = 0.00385
+u = np.array([0.00385])
+
+# pos_change = 0
+# speed_change = dt*acceleration
+
+# B_ground_truth * u = |0 | |a| = | 0  |
+#                      |dt|       |dt*a|
+B_ground_truth = np.array([[0],
+                           [DT]])
+
+# process noise
+Q_ground_truth = np.array([[1.0, 0.0],
+                           [0.0, 0.01]])
+                          
+H_ground_truth = np.array([[0.98, 0.0],
+                           [0.0, 1.02]])
+
+R_ground_truth = np.array([[100.0, 0.0],
+                           [0.0,       1.0]])
